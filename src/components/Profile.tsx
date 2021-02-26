fe8a699963b65f910b4c3693c8975bbbb2333a76
Pedro Brasca <pedropaulobrasca@gmail.com>
2021-02-25 22:01:22 -0300

    Aula 04 finalizada

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.container}>
       <img src="https://github.com/pedropaulobrasca.png" alt="Pedro Paulo" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Pedro Paulo</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
